1895205b3eb9787ef4c3343710aa205ea81b8f7b
yaoxuchen <yaoxuchen@2718ad2c-20e5-11df-b3bf-63d83f449e3e>
2010-02-24 10:55:52 +0000

    serializeXML mostly done

--- MrsQG/src/de/uni_saarland/coli/MrsQG/ElementaryPredication.java
+++ MrsQG/src/de/uni_saarland/coli/MrsQG/ElementaryPredication.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.TreeMap;
 
 import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
 
 
 public class ElementaryPredication {
@@ -21,6 +24,7 @@ public class ElementaryPredication {
 	public String pred = null;
 	public String spred = null;
 	public String label = null;
+	public String label_vid = null;
 	public ArrayList<FvPair> fvpair = null;
 	public FvPair currentFvPair = null;
 
@@ -33,6 +37,29 @@ public class ElementaryPredication {
 		public String rargname = null;
 		public String constant = null;
 		public Var var = null;
+		
+		public void serializeXML (ContentHandler hd) {
+			AttributesImpl atts = new AttributesImpl();
+			try {
+				// <rargname>ARG0</rargname>
+				atts.clear();
+				hd.startElement("", "", "rargname", atts);
+				hd.characters(rargname.toCharArray(), 0, rargname.length());
+				hd.endElement("", "", "rargname");
+				
+				if (var!=null) {
+					var.serializeXML(hd);
+				} else if (constant != null) {
+					// <constant>John</constant>
+					atts.clear();
+					hd.startElement("", "", "constant", atts);
+					hd.characters(constant.toCharArray(), 0, constant.length());
+					hd.endElement("", "", "constant");
+				}
+			} catch (SAXException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public ElementaryPredication() {
@@ -59,7 +86,8 @@ public class ElementaryPredication {
 						"complete your code!");
 			}
 		} else if (qName.equals("label")) {
-			label = "h"+atts.getValue("vid");
+			label_vid = atts.getValue("vid");
+			label = "h"+label_vid;
 		} else if (qName.equals("fvpair")) {
 			currentFvPair = new FvPair();
 			fvpair.add(currentFvPair);
@@ -88,5 +116,54 @@ public class ElementaryPredication {
 		} else if (qName.equals("value")) {
 			currentFvPair.var.updateValue(str);			
 		}
+	}
+	
+	public void serializeXML (ContentHandler hd) {
+//		<!ELEMENT ep ((pred|realpred), label, fvpair*)>
+//		<!ATTLIST ep
+//		          cfrom CDATA #IMPLIED
+//		          cto   CDATA #IMPLIED 
+//		          surface   CDATA #IMPLIED
+//		      base      CDATA #IMPLIED >
+		AttributesImpl atts = new AttributesImpl();
+		try {
+			// <ep cfrom='0' cto='3'>
+			atts.addAttribute("", "", "cfrom", "CDATA", Integer.toString(cfrom));
+			atts.addAttribute("", "", "cto", "CDATA", Integer.toString(cto));
+			if (base!=null)
+				atts.addAttribute("", "", "surface", "CDATA", surface);
+			if (base!=null)
+				atts.addAttribute("", "", "base", "CDATA", base);
+			hd.startElement("", "", "ep", atts);
+			
+			if (pred!=null) {
+				//<pred>PROPER_Q_REL</pred>
+				atts.clear();
+				hd.startElement("", "", "pred", atts);
+				hd.characters(pred.toCharArray(), 0, pred.length());
+				hd.endElement("", "", "pred");
+			} else if (spred!=null) {
+				//<spred>_like_v_1_rel</pred>
+				atts.clear();
+				hd.startElement("", "", "spred", atts);
+				hd.characters(spred.toCharArray(), 0, spred.length());
+				hd.endElement("", "", "spred");
+			}
+			
+			//<label vid='3'/>
+			atts.clear();
+			atts.addAttribute("", "", "vid", "CDATA", label_vid);
+			hd.startElement("", "", "label", atts);
+			hd.endElement("", "", "label");
+			
+			//<fvpair>
+			for (FvPair p : fvpair) {
+				p.serializeXML(hd);
+			}
+			hd.endElement("", "", "ep");
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
